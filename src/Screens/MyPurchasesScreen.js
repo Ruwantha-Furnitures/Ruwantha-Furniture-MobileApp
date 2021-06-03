10c6039400810d858671169e57e5a85f72732744
Ayomal98 <ayomalpraveen33@gmail.com>
2021-06-04 04:38:54 +0530

    Header and Card  component added

---
+++ src/Screens/MyPurchasesScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+const MyPurchasesScreen = () => {
+  return (
+    <View>
+      <Text>This Page Contains all the Products I Bought</Text>
+    </View>
+  );
+};
+
+export default MyPurchasesScreen;
---
+++ src/Screens/ViewProductARScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+const ViewProductARScreen = () => {
+  return (
+    <View>
+      <Text>AR Camera Screen Page</Text>
+    </View>
+  );
+};
+
+export default ViewProductARScreen;
