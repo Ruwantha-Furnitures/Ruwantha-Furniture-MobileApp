134ca3dced3d956842379f5bcc1106ffdef34715
Ayomal98 <ayomalpraveen33@gmail.com>
2021-06-28 00:00:34 +0530

    backend started & database connection added

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,34 @@
 //this is the server file
+
+const express = require("express");
+const app = express();
+
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "ar_magic",
+});
+
+const {customerRouter}=require("./routes/customers");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use("/customer",customerRouter);
+
+async function connectDB() {
+    try {
+        await connection.connect();
+        console.log("Connected to Database")
+    } catch (error) {
+        console.log("error")
+    }
+}
+
+app.listen(3000, () => {
+  console.log("Application is running on the port 3000");
+  connectDB();
+});
