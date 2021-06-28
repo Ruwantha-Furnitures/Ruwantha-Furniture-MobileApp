a175bb12af242cbfe5524f1d44b0dc907c4110b0
Ayomal98 <ayomalpraveen33@gmail.com>
2021-06-28 21:01:44 +0530

    state handling added for signup

--- backend/controllers/CustomerController.js
+++ backend/controllers/CustomerController.js
@@ -1,19 +1,19 @@
 const mysql = require("mysql");
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "ar_magic",
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "armagic",
+});
+
+const getCustomersController = (req, res) => {
+  db.query("SELECT * FROM customer", (error, result, fields) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(result);
+    }
   });
+};
 
-const getCustomersController=(req,res) => {
-    connection.query("SELECT * FROM customer",(error,result,fields)=>{
-        if(error){
-            console.log(error);
-        }else{
-            console.log(result);
-        }
-    })
-}
-
-module.exports={getCustomersController}
+module.exports = { getCustomersController };
--- backend/server.js
+++ backend/server.js
@@ -8,24 +8,23 @@ const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "ar_magic",
+  database: "armagic",
 });
 
-const {customerRouter}=require("./routes/customers");
+const { customerRouter } = require("./routes/customers");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.use("/customer",customerRouter);
+app.use("/customer", customerRouter);
 
 async function connectDB() {
-    try {
-        await connection.connect();
-        console.log("Connected to Database")
-    } catch (error) {
-        console.log("error")
-    }
+  try {
+    await connection.connect();
+    console.log("Connected to Database");
+  } catch (error) {
+    console.log("error");
+  }
 }
 
 app.listen(3000, () => {
