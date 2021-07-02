a0417a690d0a089e7aa7e610077be2cdbf88a693
Ayomal98 <ayomalpraveen33@gmail.com>
2021-07-03 00:47:15 +0530

    login controller added

--- backend/controllers/registration/LoginController.js
+++ backend/controllers/registration/LoginController.js
@@ -1,35 +1,41 @@
+const { Account } = require("../../models");
 const mysql = require("mysql");
 const bcrypt = require("bcrypt");
 const saltrounds = 10;
 
-const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "armagic",
-});
-
-const LoginController = (req, res) => {
+const LoginController = async (req, res) => {
   const { password, userEmail } = req.body.data;
+  const userlevel = 1;
+  const data = { email: userEmail, userlevel };
 
-  const selectQuery = "SELECT * FROM account WHERE email=?";
-  db.query(selectQuery, [userEmail], (error, result) => {
-    const hashedPassword = result[0].password;
-    bcrypt.compare(password, hashedPassword, (errdecrypt, resultdecrypt) => {
-      if (resultdecrypt) {
-        res.send({
-          state: "Successful",
-          message: "Login Succcessful",
-        });
-      } else {
-        res.send({
-          state: "Error",
-          errorMessage:
-            "We couldn’t find an account matching the email and password you entered. Please check your email and password and try again.",
-        });
-      }
-    });
-  });
+  try {
+    const account = await Account.findOne({ data });
+    if (account === null) {
+      res.send({
+        state: "Error",
+        errorMessage:
+          "We couldn’t find an account matching the email and password you entered. Please check your email and password and try again.",
+      });
+    } else {
+      const existingPassword = account.password;
+      bcrypt.compare(password, existingPassword, (error, result) => {
+        if (result) {
+          res.send({
+            state: "Successful",
+            message: "Login Succcessful",
+          });
+        } else {
+          res.send({
+            state: "Error",
+            errorMessage:
+              "We couldn’t find an account matching the email and password you entered. Please check your email and password and try again.",
+          });
+        }
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = { LoginController };
