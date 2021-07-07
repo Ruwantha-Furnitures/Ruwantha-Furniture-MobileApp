e6fd689b93761924610941bd63321d135ab9e67f
Nuwan Fernando <lifeofhassa@outlook.com>
2021-07-07 18:50:21 +0530

    Checkoutform

---
+++ frontend/src/Components/Screen/Cart/CheckOutForm.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, Text, StyleSheet, Dimensions, StatusBar } from "react-native";
+import Card from "../../UI/Card";
+import UserDetails from "./UserDetails";
+
+const mobileWidth = Dimensions.get("window").width;
+const mobileHeight = Dimensions.get("window").height;
+const StatusBarHeight = StatusBar.currentHeight;
+
+const CheckOutForm = () => {
+  return (
+    <Card
+      width={mobileWidth - 40}
+      height={mobileHeight / 1.4}
+      ml={20}
+      bg="#fff"
+    >
+      <UserDetails />
+    </Card>
+  );
+};
+
+export default CheckOutForm;
