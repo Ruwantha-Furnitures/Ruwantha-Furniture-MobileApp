1e2e07c6743ce9af018a5b322da23c69340f0330
Ayomal98 <ayomalpraveen33@gmail.com>
2021-07-12 14:26:38 +0530

    image gallery done

--- frontend/src/Components/Screen/Home/CustomIntro.js
+++ frontend/src/Components/Screen/Home/CustomIntro.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, FlatList } from "react-native";
 import Card from "../../UI/Card";
+import SubHeader from "../../Header/SubHeader";
 const CustomIntro = () => {
   const Images = [
     {
       id: 1,
-      imgUri: require("../../../../assets/1.png"),
+      imgUri: require("../../../../assets/7.jpeg"),
     },
     {
       id: 2,
@@ -13,7 +14,7 @@ const CustomIntro = () => {
     },
     {
       id: 3,
-      imgUri: require("../../../../assets/3.jpeg"),
+      imgUri: require("../../../../assets/1.png"),
     },
     {
       id: 4,
@@ -26,19 +27,28 @@ const CustomIntro = () => {
   ];
 
   return (
-    <Card width={415} height={310} ml={20} bg="#fff">
+    <Card width={415} height={340} ml={20} bg="#fff">
+      <SubHeader title="Image Gallery" width={290} />
       <FlatList
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
         data={Images}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
-          <View style={{ width: 415, height: 310 }}>
+          <View>
             <Image
               source={item.imgUri}
-              style={[StyleSheet.absoluteFillObject]}
-            />{" "}
+              style={{
+                width: 415,
+                height: 272,
+                resizeMode: "cover",
+                borderBottomLeftRadius: 10,
+                borderBottomRightRadius: 10,
+              }}
+            />
           </View>
         )}
-        resizeMode="stretch"
       />
     </Card>
   );
