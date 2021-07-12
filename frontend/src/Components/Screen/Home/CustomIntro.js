2d44215bf17b63d6f4b711ef41e6f06768ccc48b
Ayomal98 <ayomalpraveen33@gmail.com>
2021-07-12 14:00:29 +0530

    image gallery array

--- frontend/src/Components/Screen/Home/CustomIntro.js
+++ frontend/src/Components/Screen/Home/CustomIntro.js
@@ -2,6 +2,29 @@ import React from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
 import Card from "../../UI/Card";
 const CustomIntro = () => {
+  const Images = [
+    {
+      id: 1,
+      imgUri: require("../../../../assets/1.png"),
+    },
+    {
+      id: 2,
+      imgUri: require("../../../../assets/2.jpeg"),
+    },
+    {
+      id: 3,
+      imgUri: require("../../../../assets/3.jpeg"),
+    },
+    {
+      id: 4,
+      imgUri: require("../../../../assets/4.jpeg"),
+    },
+    {
+      id: 5,
+      imgUri: require("../../../../assets/5.jpeg"),
+    },
+  ];
+
   return (
     <Card width={415} height={310} ml={20} bg="#fff">
       <Image
