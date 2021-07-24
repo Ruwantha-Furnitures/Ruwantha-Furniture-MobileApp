09638c4a0be898738d9153030aa066977475d630
MedaniGunathilaka <hansikamedani@gmail.com>
2021-07-24 20:05:58 +0530

    Inserted header to home screen

--- frontend/src/Screens/DeliveryDriver/HomeScreen.js
+++ frontend/src/Screens/DeliveryDriver/HomeScreen.js
@@ -1,32 +1,38 @@
 //This is the landing page
 import React from "react";
-import { View, Text, StyleSheet,Image,TouchableOpacity,Dimensions, } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
 import Card from "../../Components/UI/Card";
+import Header from "../../Components/Header/Header";
 import { MaterialIcons } from "@expo/vector-icons";
 const HomeScreen = () => {
   const mobileWidth = Dimensions.get("window").width;
   const mobileHeight = Dimensions.get("window").height;
   return (
-    <Card
-      width={mobileWidth - 60}
-      height={mobileHeight / 6}
-      ml={30}
-      pd={7}
-      fd="row"
-      bg="#FFF"
-    >
-    
     <View style={styles.productContainer}>
+      <Header />
+      <Card
+        width={mobileWidth - 60}
+        height={mobileHeight / 6}
+        ml={30}
+        pd={7}
+        fd="row"
+        bg="#FFF"
+      >
         <View style={styles.itemDetailsContainer}>
-          <Text style={styles.itemName}>{item.name}</Text>
-            <Text style={styles.itemPrice}>{`${item.price} /=`}</Text>
-              <Text style={styles.itemQuantity}>1</Text>
+          <Text style={styles.itemName}>Item Name</Text>
+          <Text style={styles.itemPrice}>Rs. 2000 /=</Text>
+          <Text style={styles.itemQuantity}>1</Text>
         </View>
+      </Card>
     </View>
-    </Card>
 
-    
- 
   );
 };
 
@@ -45,15 +51,13 @@ const styles = StyleSheet.create({
     marginLeft: 30,
     flexDirection: "column",
   },
-  
+
   itemPrice: {
     marginTop: 10,
     color: "#FB9F3C",
     fontSize: 16,
     fontWeight: "bold",
   },
-  
 });
 
 export default HomeScreen;
-
