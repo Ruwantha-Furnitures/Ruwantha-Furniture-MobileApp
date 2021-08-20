3889f6a594dfe8ac5673d243612b088e4b1064f9
Nuwan Fernando <lifeofhassa@outlook.com>
2021-08-20 15:48:49 +0530

    View order screen

--- frontend/src/Screens/DeliveryDriver/ViewOrdersScreen.js
+++ frontend/src/Screens/DeliveryDriver/ViewOrdersScreen.js
@@ -1,36 +1,89 @@
 //viewing all the orders for today
+//ViewOrdersScreen.js
+import React, { useContext } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  Image,
+} from "react-native";
+import Order from "../../Components/Screen/DeliveryDriver/Order";
+import { LoginContext } from "../../Components/Reducers/loginReducer";
+import AvailabilityStatus from "../../Components/Screen/DeliveryDriver/AvailabilityStatus";
 
-import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 const ViewOrdersScreen = ({ navigation }) => {
+  const loginContext = useContext(LoginContext);
+  const orders = [
+    {
+      id: 1,
+      orderID: "OD001",
+      customerName: "Ayomal Praveen",
+      address: "No.54, Negombo Road, Dankotuwa",
+      telephoneNumber: "0776054853",
+      status: "Not Delivered",
+      totalAmount: "Rs.10,000",
+      purchasedDate: "2021-10-20",
+      dueDate: "2021-10-22",
+      contactNumber: "0777604473",
+    },
+    {
+      id: 2,
+      orderID: "OD002",
+      customerName: "Amal Perera",
+      address: "No.14, Colombo Road, Negombo",
+      telephoneNumber: "0776029808",
+      status: "Delivered",
+      totalAmount: "Rs.20,000",
+      purchasedDate: "2021-10-21",
+      dueDate: "2021-10-23",
+      contactNumber: "0777605453",
+    },
+    {
+      id: 3,
+      orderID: "OD003",
+      customerName: "Nimal Fernando",
+      address: "No.94, Kandy Road, Kurunegala",
+      telephoneNumber: "0777604473",
+      status: "Delivered",
+      totalAmount: "Rs.30,000",
+      purchasedDate: "2021-10-19",
+      dueDate: "2021-10-21",
+      contactNumber: "0753604473",
+    },
+  ];
   return (
     <View style={styles.viewContainer}>
-      <Text style={styles.header}>Order-Details</Text>
-      <View>
-        <Text>Order 1</Text>
-        <TouchableOpacity onPress={() => navigation.navigate("OrderDetails")}>
-          <Text>More Details</Text>
-        </TouchableOpacity>
-      </View>
-      <View>
-        <Text>Order 2</Text>
-        <TouchableOpacity onPress={() => navigation.navigate("OrderDetails")}>
-          <Text>More Details</Text>
-        </TouchableOpacity>
-      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flexDirection: "row",
+            marginTop: 15,
+            marginRight: 10,
+            justifyContent: "space-between",
+          }}
+        >
+          <AvailabilityStatus />
+          <TouchableOpacity
+            style={styles.buttonLg}
+            onPress={() => loginContext.loginDispatch({ type: "logout" })}
+          >
+            <Text style={styles.Login}>Logout</Text>
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.header}>Order Details</Text>
+        <Image
+          source={require("../../../assets/nlogo.png")}
+          style={styles.imageHeader}
+        />
+        {orders.map((order) => (
+          <Order order={order} navigation={navigation} />
+        ))}
+      </ScrollView>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  viewContainer: {
-    flex: 1,
-  },
-  header: {
-    fontWeight: "100",
-    fontWeight: "bold",
-    fontSize: 20,
-    alignSelf: "center",
-  },
-});
+
 export default ViewOrdersScreen;
