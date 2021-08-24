c50afeec627fb4f141329ae0fdd097fa1d7a8a07
Ayomal98 <ayomalpraveen33@gmail.com>
2021-08-24 16:55:34 +0530

    delivery charges api done

--- backend/controllers/products/payment.controller.js
+++ backend/controllers/products/payment.controller.js
@@ -1,7 +1,15 @@
 const db = require("../../models");
 const DeliveryCharges = db.deliveryCharges;
 
-const getAllDistrictsChargesController = async (req, res) => {};
+const getAllDistrictsChargesController = async (req, res) => {
+  try {
+    const deliveryCharges = await DeliveryCharges.findAll();
+    console.log(deliveryCharges);
+    res.status(200).json({ deliveryCharges });
+  } catch (error) {
+    res.status(500).json({ message: "Error while retrieving payment Details" });
+  }
+};
 
 const shippingDetailsController = async (req, res) => {};
 
