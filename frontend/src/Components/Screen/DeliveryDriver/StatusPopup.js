75291e830106653858ca1b4adce015e8df335290
Nuwan Fernando <lifeofhassa@outlook.com>
2021-09-01 17:25:22 +0530

    status pop done

--- frontend/src/Components/Screen/DeliveryDriver/StatusPopup.js
+++ frontend/src/Components/Screen/DeliveryDriver/StatusPopup.js
@@ -4,7 +4,8 @@ import PopUpConfirmationModal from "../../UI/PopUpConfirmationModal";
 import FormAppButton from "../../UI/FormAppButton";
 import { AntDesign } from "@expo/vector-icons";
 
-const StatusPopup = ({ showModal, deleteHandler }) => {
+const StatusPopup = ({ showModal, deleteHandler, changeStatus, order_id }) => {
+  
   return (
     <PopUpConfirmationModal visible={showModal}>
       <AntDesign
@@ -19,29 +20,40 @@ const StatusPopup = ({ showModal, deleteHandler }) => {
       </Text>
       <View style={styles.btnContainer}>
         <FormAppButton title="No" width={90} onPress={deleteHandler} />
-        <FormAppButton title="Yes" type="Submit" width={90} />
+        <FormAppButton
+          title="Yes"
+          type="Submit"
+          width={90}
+          onPress={() => {
+            deleteHandler();
+            changeStatus(order_id);
+          }}
+        />
       </View>
     </PopUpConfirmationModal>
   );
 };
 
-const styles = StyleSheet.create({
-  closeIcon: {
-    alignSelf: "flex-end",
-    marginTop: -18,
-    marginRight: 5,
-    marginBottom: 0,
-  },
-  confirmationText: {
-    fontSize: 17,
-    fontWeight: "bold",
-    marginTop: 25,
-  },
-  btnContainer: {
-    justifyContent: "center",
-    flexDirection: "row",
-    marginTop: 30,
-    marginBottom: 10,
-  },
-});
-export default StatusPopup;
+// //commit 2 line 39-57 style added
+// const styles = StyleSheet.create({
+//   closeIcon: {
+//     alignSelf: "flex-end",
+//     marginTop: -18,
+//     marginRight: 5,
+//     marginBottom: 0,
+//   },
+//   confirmationText: {
+//     fontSize: 17,
+//     fontWeight: "bold",
+//     marginTop: 25,
+//   },
+//   btnContainer: {
+//     justifyContent: "center",
+//     flexDirection: "row",
+//     marginTop: 30,
+//     marginBottom: 10,
+//   },
+// });
+// export default StatusPopup;
+
+// //after all commits are finished above(copy paste whole page-without the comments) : commit 3 "StatusPopup done"
