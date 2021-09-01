7adc3ee24bb3b5688ec6daffbe076fa66358bd4a
MedaniGunathilaka <hansikamedani@gmail.com>
2021-09-01 10:01:21 +0530

    State hadnling fro HomeScreen done

--- frontend/src/Screens/DeliveryDriver/HomeScreen.js
+++ frontend/src/Screens/DeliveryDriver/HomeScreen.js
@@ -1,6 +1,8 @@
 //This is the landing page
 //HomeScreen.js file
-import React, { useContext } from "react";
+//path:frontend/Screens/DeliveryDriver/HomeScreen.js
+
+import React, { useContext, useEffect, useState } from "react";
 import {
   View,
   Text,
@@ -14,179 +16,29 @@ import Card from "../../Components/UI/Card";
 import Header from "../../Components/Header/Header";
 import { MaterialIcons } from "@expo/vector-icons";
 import { LoginContext } from "../../Components/Reducers/loginReducer";
+import { DriverContext } from "../../Components/Reducers/driverReducer";
 import AvailabilityStatus from "../../Components/Screen/DeliveryDriver/AvailabilityStatus";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { FontAwesome5 } from "@expo/vector-icons";
 import PendingOrdersTable from "../../Components/Screen/DeliveryDriver/PendingOrdersTable";
+import axios from "axios";
+import { API_URL } from "react-native-dotenv";
+import * as SecureStore from "expo-secure-store";
 
-const HomeScreen = () => {
-  const mobileWidth = Dimensions.get("window").width;
-  const mobileHeight = Dimensions.get("window").height;
+const mobileHeight = Dimensions.get("window").height;
+const mobileWidth = Dimensions.get("window").width;
+
+const HomeScreen = ({ navigation }) => {
+
   const loginContext = useContext(LoginContext);
+  const driverContext = useContext(DriverContext);
+  const [todayAssigned, setTodayAssigned] = useState(null);
+  const [todayPending, setTodayPending] = useState(null);
+  const [todayCompleted, setTodayCompleted] = useState(null);
+  const [monthlyCompleted, setMonthlyCompleted] = useState(null);
+  const [pendingOrders, setPendingOrders] = useState([]);
 
-  return (
-    <ScrollView showsVerticalScrollIndicator={false}>
-      <View style={styles.productContainer}>
-        <View
-          style={{
-            flexDirection: "row",
-            marginTop: 15,
-            marginRight: 10,
-            justifyContent: "space-between",
-          }}
-        >
-          <AvailabilityStatus />
-          <TouchableOpacity
-            style={styles.buttonLg}
-            onPress={() => loginContext.loginDispatch({ type: "logout" })}
-          >
-            <Text style={styles.Login}>Logout</Text>
-          </TouchableOpacity>
-        </View>
-        <Text style={styles.header}>Home Page</Text>
-        <Image
-          source={require("../../../assets/nlogo.png")}
-          style={styles.imageHeader}
-        />
-        <View style={{ flexDirection: "row" }}>
-          <Card
-            width={mobileWidth / 2.15}
-            height={mobileHeight / 6}
-            ml={10}
-            pd={7}
-            fd="row"
-            bg="#FFF"
-          >
-            <View style={styles.itemDetailsContainer}>
-              <MaterialCommunityIcons
-                name="truck-delivery"
-                size={40}
-                color="#542b14"
-              />
-              <Text style={styles.itemName}>Today Sales</Text>
-              <Text style={styles.itemPrice}>02</Text>
-            </View>
-          </Card>
-          <Card
-            width={mobileWidth / 2.15}
-            height={mobileHeight / 6}
-            ml={10}
-            pd={7}
-            fd="row"
-            bg="#FFF"
-          >
-            <View style={styles.itemDetailsContainer}>
-              <MaterialIcons name="category" size={40} color="#542b14" />
-              <Text style={styles.itemName}>Sales Items</Text>
-              <Text style={styles.itemPrice}>50</Text>
-            </View>
-          </Card>
-        </View>
-        <View style={{ flexDirection: "row" }}>
-          <Card
-            width={mobileWidth / 2.15}
-            height={mobileHeight / 6}
-            ml={10}
-            pd={7}
-            fd="row"
-            bg="#FFF"
-          >
-            <View style={styles.itemDetailsContainer}>
-              <MaterialIcons
-                name="stacked-line-chart"
-                size={40}
-                color="#542b14"
-              />
-              <Text style={styles.itemName}>New Orders</Text>
-              <Text style={styles.itemPrice}>Rs.8000 /=</Text>
-            </View>
-          </Card>
 
-          <Card
-            width={mobileWidth / 2.15}
-            height={mobileHeight / 6}
-            ml={10}
-            pd={7}
-            fd="row"
-            bg="#FFF"
-          >
-            <View style={styles.itemDetailsContainer}>
-              <FontAwesome5 name="spa" size={40} color="#542b14" />
-              <Text style={styles.itemName}>Total Orders</Text>
-              <Text style={styles.itemPrice}>Rs.25,000 /=</Text>
-            </View>
-          </Card>
-        </View>
-        <PendingOrdersTable />
-      </View>
-    </ScrollView>
-  );
-};
-
-const styles = StyleSheet.create({
-  itemImage: {
-    width: 90,
-    height: 90,
-    borderRadius: 40,
-  },
-
-  itemName: {
-    marginTop: 10,
-    fontSize: 18,
-    width: 180,
-    letterSpacing: 2,
-  },
-
-  productContainer: {
-    flex: 1,
-    backgroundColor: "#E7E5E9",
-  },
-  itemDetailsContainer: {
-    marginLeft: 30,
-    flexDirection: "column",
-  },
-
-  itemPrice: {
-    marginTop: 10,
-    color: "#542b14",
-    fontSize: 25,
-    fontWeight: "bold",
-  },
-
-  buttonLg: {
-    backgroundColor: "black",
-    borderRadius: 20,
-    paddingRight: 0,
-    paddingLeft: 17,
-    paddingVertical: 12,
-    marginTop: 3,
-    justifyContent: "center",
-  },
-  Login: {
-    fontWeight: "bold",
-    fontSize: 15,
-    color: "#fff",
-    letterSpacing: 1,
-    width: 75,
-  },
-  header: {
-    fontWeight: "100",
-    fontWeight: "bold",
-    fontSize: 35,
-    alignSelf: "center",
-    marginTop: 12,
-    width: 300,
-    marginBottom: 10,
-    letterSpacing: 5,
-    marginLeft: 50,
-  },
-  imageHeader: {
-    width: 40,
-    height: 40,
-    marginRight: 5,
-    backgroundColor: "#E7E5E9",
-    alignSelf: "center",
-  },
-});
+}
 
 export default HomeScreen;
