a2e33fe0809e0ca8ff421093b8db10152bbad5a1
Nuwan Fernando <lifeofhassa@outlook.com>
2021-09-01 17:19:16 +0530

    Order More Details component done

--- frontend/src/Components/Screen/DeliveryDriver/OrderMoreDetails.js
+++ frontend/src/Components/Screen/DeliveryDriver/OrderMoreDetails.js
@@ -129,5 +129,3 @@ const styles = StyleSheet.create({
   },
 });
 export default OrderMoreDetails;
-
-//after all commits are finished above(copy paste whole page-without the comments) : commit 4 "Order More Details component done"
