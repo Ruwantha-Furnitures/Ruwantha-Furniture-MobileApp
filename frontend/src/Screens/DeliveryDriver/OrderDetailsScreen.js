f9bfa7481713d9e9c9d1a4918946019c061369b0
Nuwan Fernando <lifeofhassa@outlook.com>
2021-09-01 16:04:20 +0530

    state handling done for orders

--- frontend/src/Screens/DeliveryDriver/OrderDetailsScreen.js
+++ frontend/src/Screens/DeliveryDriver/OrderDetailsScreen.js
@@ -1,25 +1,94 @@
-//More details on the order
-//OrderDetailsScreen.js
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, ScrollView } from "react-native";
 import OrderMoreDetails from "../../Components/Screen/DeliveryDriver/OrderMoreDetails";
 import PurchasedProductTable from "../../Components/Screen/DeliveryDriver/PurchasedProductTable";
-const OrderDetailsScreen = ({ route }) => {
-  const { order } = route.params;
-  console.log(order);
-  return (
-    <ScrollView showsVerticalScrollIndicator={false}>
-      <View style={styles.viewContainer}>
-        <OrderMoreDetails order={order} />
-      </View>
-    </ScrollView>
-  );
-};
+import axios from "axios";
+import { API_URL } from "react-native-dotenv";
 
-const styles = StyleSheet.create({
-  viewContainer: {
-    flex: 1,
-    alignItems: "center",
-  },
-});
+const OrderDetailsScreen = ({ route }) => {
+  
+  const { order } = route.params;
+  const [moreDetails, setMoreDetails] = useState(null);
+  const [productContainer, setProductContainer] = useState([]);
+  console.log(order.order_id);
+
+  //commit 2 line 21-51 fetchOrders API calling done
+//   const fetchOrderDetails = async () => {
+//     try {
+//       const {
+//         order_id,
+//         customerName,
+//         shipping_address,
+//         contact_number,
+//         total_product_amount,
+//         purchasedDate,
+//         complete_status,
+//       } = order;
+//       setMoreDetails({
+//         order_id,
+//         customerName,
+//         shipping_address,
+//         contact_number,
+//         total_product_amount,
+//         purchasedDate,
+//         complete_status,
+//       });
+//       const response = await axios.get(
+//         `${API_URL}deliveryDriver/orders/moreDetails/${order_id}`
+//       );
+//       if (response.status === 200) {
+//         setProductContainer(response.data.productContainer);
+//         console.log("done");
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   //commit 3 line 54-67 - changeStatusHandler API calling done
+//   const changeStatusHandler = async (order_id) => {
+//     try {
+//       const driverID = await SecureStore.getItemAsync("deliveryDriver_id");
+//       console.log(order_id);
+//       const response = await axios.put(
+//         `${API_URL}deliveryDriver/orders/changeStatus/${order_id}/${driverID}`
+//       );
+//       if (response.status === 200) {
+//         console.log(response.data);
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   //commit 4 line 70-72 - fetch order function calling done inside useeffect
+//   useEffect(() => {
+//     fetchOrderDetails();
+//   }, []);
+
+//   //commit 5 line 74-88 -order retreival added
+//   return (
+//     <ScrollView showsVerticalScrollIndicator={false}>
+//       <View style={styles.viewContainer}>
+//         {productContainer.length > 0 && (
+//           <OrderMoreDetails
+//             order={moreDetails}
+//             productContainer={productContainer}
+//             changeStatus={changeStatusHandler}
+//           />
+//         )}
+//       </View>
+//     </ScrollView>
+//   );
+// };
+
+// //commit 6 line 91-98 -order retreival added
+// const styles = StyleSheet.create({
+//   viewContainer: {
+//     flex: 1,
+//     alignItems: "center",
+//   },
+};
 export default OrderDetailsScreen;
+
+//after all commits are finished above(copy paste whole page-without the comments) : commit 7 "Order Details Screen done"
