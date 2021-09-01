945a1d71a00df94f8cccf8847e83d9f6d9560b78
Nuwan Fernando <lifeofhassa@outlook.com>
2021-09-01 17:21:59 +0530

    purchased product table added with the use of productdata

--- frontend/src/Components/Screen/DeliveryDriver/PurchasedProductTable.js
+++ frontend/src/Components/Screen/DeliveryDriver/PurchasedProductTable.js
@@ -3,84 +3,100 @@ import { View, Text, StyleSheet, Dimensions } from "react-native";
 import { Col, Row, Grid } from "react-native-easy-grid";
 import Card from "../../UI/Card";
 
-const PurchasedProductTable = () => {
+const PurchasedProductTable = ({ productContainer }) => {
   const mobileWidth = Dimensions.get("window").width;
   const cardWidth = mobileWidth - 40;
   return (
     <React.Fragment>
       <Text style={styles.subheader}>Purchased Product Details</Text>
-
-      <Grid style={{ marginHorizontal: 10, marginVertical: 10 }}>
-        <Col size={25}>
-          <Row style={styles.cell}>
-            <Text
-              style={{
-                fontWeight: "bold",
-                height: "100%",
-              }}
-            >
-              Product Name
-            </Text>
-          </Row>
-          <Row style={styles.cell}>
-            <Text>Serena Single Seater</Text>
-          </Row>
-        </Col>
-        <Col size={15}>
-          <Row style={styles.cell}>
-            <Text
-              style={{
-                fontWeight: "bold",
-                width: 80,
-                alignSelf: "flex-start",
-              }}
-            >
-              Quantity
-            </Text>
-          </Row>
-          <Row style={styles.cell}>
-            <Text>1</Text>
-          </Row>
-        </Col>
-        <Col size={26}>
-          <Row style={styles.cell}>
-            <Text
-              style={{
-                fontWeight: "bold",
-                width: 80,
-                alignSelf: "flex-start",
-              }}
-            >
-              Amount
-            </Text>
-          </Row>
-          <Row style={styles.cell}>
-            <Text style={{ marginLeft: 10 }}>Rs.72975.00/=</Text>
-          </Row>
-        </Col>
-      </Grid>
+      <View>
+        <Grid
+          style={{
+            marginHorizontal: 10,
+            marginVertical: 10,
+          }}
+        >
+          <Col size={22}>
+            <Row style={styles.cell}>
+              <Text
+                style={{
+                  fontWeight: "bold",
+                  height: "100%",
+                }}
+              >
+                Product Name
+              </Text>
+            </Row>
+            {productContainer.map((product, index) => (
+              <Row style={styles.cell} key={index}>
+                <Text>{product.name}</Text>
+              </Row>
+            ))}
+          </Col>
+          <Col size={15}>
+            <Row style={styles.cell}>
+              <Text
+                style={{
+                  fontWeight: "bold",
+                  width: 80,
+                  alignSelf: "flex-start",
+                }}
+              >
+                Quantity
+              </Text>
+            </Row>
+            {productContainer.map((product, index) => (
+              <Row style={styles.cell} key={index}>
+                <Text>{product.quantity}</Text>
+              </Row>
+            ))}
+          </Col>
+          <Col size={30}>
+            <Row style={styles.cell}>
+              <Text
+                style={{
+                  fontWeight: "bold",
+                  width: 80,
+                  alignSelf: "flex-start",
+                }}
+              >
+                Amount(After Discount)
+              </Text>
+            </Row>
+            {productContainer.map((product, index) => (
+              <Row style={styles.cell} key={index}>
+                <Text>{product.itemPriceAfterDiscount}</Text>
+              </Row>
+            ))}
+          </Col>
+        </Grid>
+      </View>
     </React.Fragment>
   );
 };
 
-const styles = StyleSheet.create({
-  subheader: {
-    fontWeight: "bold",
-    marginLeft: 20,
-    fontSize: 20,
-    marginTop: 20,
-    letterSpacing: 0.5,
-    width: 300,
-    marginBottom: 20,
-    textDecorationLine: "underline",
-  },
-  cell: {
-    borderWidth: 1,
-    borderColor: "#ddd",
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    paddingHorizontal: 0,
-  },
-});
-export default PurchasedProductTable;
+// //commit 2 line 80-101 - styles done
+// const styles = StyleSheet.create({
+//   subheader: {
+//     fontWeight: "bold",
+//     marginLeft: 20,
+//     fontSize: 20,
+//     marginTop: 20,
+//     letterSpacing: 0.5,
+//     width: 300,
+//     marginBottom: 20,
+//     textDecorationLine: "underline",
+//   },
+//   cell: {
+//     borderWidth: 1,
+//     borderColor: "#ddd",
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     paddingHorizontal: 0,
+//     minHeight: 55,
+//   },
+// });
+// export default PurchasedProductTable;
+
+//after all commits are finished above(copy paste whole page-without the comments) : commit 3 "Purchased product table Screen done"
